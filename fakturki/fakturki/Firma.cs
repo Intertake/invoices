ace19c5216a1beb0919ad39aafd852cca250391f
DESKTOP-PPD4EGU\Filip <leciej.filip@gmail.com>
2020-12-06 20:45:26 +0100

    Creating new project

---
+++ fakturki/fakturki/Firma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fakturki
+{
+    class Firma
+    {
+        public int Id_firmy;
+        public string Nazwa;
+            public int NIP;
+
+        public void dodajFirme()
+        {
+            
+        }
+        public void usunFirme(Firma firma)
+        {
+
+        }
+        public void edytujFirme(Firma firma)
+        {
+
+        }
+    }
+}
---
+++ fakturki/fakturki/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fakturki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Application.Current.MainWindow.Height = SystemParameters.PrimaryScreenHeight;
+            //Application.Current.MainWindow.Width = SystemParameters.PrimaryScreenWidth;
+            Application.Current.MainWindow.Height = 1080;
+            Application.Current.MainWindow.Width = 1920;
+
+
+        }
+
+
+
+
+    }
+}
+
